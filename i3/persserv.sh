1997cf0a22fe188bdf79bffc4fc342e20b907451
Aftix <gameraftexploision@gmail.com>
2015-10-25 18:58:09 -0500

    Fixed persistent reading

--- i3/persserv.sh
+++ i3/persserv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-server=$(ls ~.i3/tempirc/ | dmenu -b -p "Choose server to read from")
-touch ~/.i3/tempirc/$server/\!me.out
-recent=$(cat ~/.i3/tempirc/$server/\!me.out | tac)
+server=$(ls ~/.i3/tempirc/ | egrep ".\.out" | dmenu -b -p "Choose server to read from")
+touch ~/.i3/tempirc/$server
+recent=$(cat ~/.i3/tempirc/$server | tac)
 echo "$recent" | dmenu -b -p 'Msgs from $server:'
