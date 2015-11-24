b591daece56d6c0d57af9e81937127ab376f519a
Aftix <gameraftexploision@gmail.com>
2015-11-23 18:40:07 -0600

    Removed workspaces 1-10 from the advanced menu

--- i3/advworkspace.sh
+++ i3/advworkspace.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 function open {
-	i3-msg -t command workspace $(i3-msg -t get_workspaces | sed "s/,/\n/g" | grep "^\"name\":" | sed "s/^\"name\":\"//" | sed "s/\"$//" | dmenu -b -p "Workspaces:")
+	i3-msg -t command workspace $(i3-msg -t get_workspaces | sed "s/,/\n/g" | grep "^\"name\":" | sed "s/^\"name\":\"//" | sed "s/\"$//" | grep -v "[0-9]" | dmenu -b -p "Workspaces:")
 }
 
 function send {
-	i3-msg -t command move workspace $(i3-msg -t get_workspaces | sed 's/,/\n/g' | grep '\"name\":' | sed 's/\"name\":\"//g' | sed 's/\"$//g' | dmenu -b -p 'Workspaces:')
+	i3-msg -t command move workspace $(i3-msg -t get_workspaces | sed 's/,/\n/g' | grep '\"name\":' | sed 's/\"name\":\"//g' | sed 's/\"$//g' | grep -v "[0-9]" | dmenu -b -p 'Workspaces:')
 }
 
 $1
