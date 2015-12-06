9bce6e093babc91fde375cad4a2fc20ea88513e5
Aftix <gameraftexploision@gmail.com>
2015-12-06 17:36:47 -0600

    Dmenu file browser

---
+++ i3/dmenufm.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -f $HOME/.config/dmenu ]; then
+	source $HOME/config/dmenu
+else
+	DMENU="dmenu -b -l 4 -fn 'Terminus-10' -sf green -sb red -nb #ffffff -nf #000000"
+fi
+
+if [[ "$1" == "" ]]; then
+	cd $(cat $HOME/.local/share/dmenufm)
+else
+	cd $1
+fi
+
+response="Null"
+
+while [[ "$response" != "" ]]; do
+	response=$(ls -a | $DMENU -p "$(pwd):" -l 20)
+	if [[ "$response" == "" ]]; then
+		break
+	elif [ -d "$response" ]; then
+		cd "$response"
+	else
+		ext=$(xdg-mime query filetype $response)
+		results=$(grep "$ext" $HOME/.local/share/dmenufm.allowed)
+		if [[ "$results" != "" ]]; then
+			xdg-open $response
+			break
+		fi
+	fi
+done
+
+echo "$(pwd)" > $HOME/.local/share/dmenufm
